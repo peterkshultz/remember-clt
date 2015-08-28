c467b7794f288fb7798ba7736f0b9d921aa2b54a
Peter Shultz <peterkshultz@verizon.net>
2015-08-28 09:00:33 -0400

    Add .go files

---
+++ remember.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+//  "encoding/json"
+  "fmt"
+//  "io/ioutil"
+//  "log"
+//  "net/http"
+//  "net/url"
+  "os"
+//  "os/user"
+//  "path/filepath"
+//  "time"
+
+//  "golang.org/x/net/context"
+//  "golang.org/x/oauth2"
+//  "golang.org/x/oauth2/google"
+  "google.golang.org/api/calendar/v3"
+  "github.com/codegangsta/cli"
+)
+
+
+func main() {
+  app := cli.NewApp()
+  app.Name = "remember"
+  app.Usage = "utilize the awesome UI that is the command line to create Google Calendar events. Maybe if you use it you'll put your life in order :)"
+  app.Action = func(c *cli.Context) {
+    println(c.Args().First())
+  }
+
+  app.Commands = []cli.Command{
+  {
+      Name: "add",
+      Usage: "add an event to the calendar",
+      Action: func(c *cli.Context) {
+  	
+    	  event := &calendar.Event{
+      	
+        Summary: c.Args().First(),
+      	Location: "",
+      	Description: c.Args().First(),
+      	Start: &calendar.EventDateTime{
+            DateTime: "2016-05-28T09:00:00-07:00",
+        	  TimeZone: "America/Detroit",
+      	},
+
+      	End: &calendar.EventDateTime{
+            DateTime: "2016-05-28T17:00:00-07:00",
+        		TimeZone: "America/Detroit",
+      		},
+      		
+    	  }
+
+    	calendarId := "primary"
+    	
+    	event = srv.Events.Insert(calendarId, event).Do()
+    	
+    	
+    	fmt.Printf("Event created: %s\n", event.HtmlLink)
+
+      },
+    },
+
+  }
+
+  app.Run(os.Args)
+}
