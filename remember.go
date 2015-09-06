05dea304ba415409ca7df503139d0139d73eaa8b
Peter Shultz <peterkshultz@verizon.net>
2015-09-05 22:41:08 -0400

    Improve readability

--- remember.go
+++ remember.go
@@ -1,60 +1,60 @@
 package main
 
 import (
-//  "encoding/json"
-  "fmt"
-//  "io/ioutil"
-//  "log"
-//  "net/http"
-//  "net/url"
-  "os"
-//  "os/user"
-//  "path/filepath"
-//  "time"
 
-//  "golang.org/x/net/context"
-  "golang.org/x/oauth2"
-  "golang.org/x/oauth2/google"
-  "google.golang.org/api/calendar/v3"
-  "github.com/codegangsta/cli"
+    "fmt"
+    "os"
+    "golang.org/x/oauth2"
+    "golang.org/x/oauth2/google"
+    "google.golang.org/api/calendar/v3"
+    "github.com/codegangsta/cli"
+
 )
 
 
 func main() {
-  app := cli.NewApp()
-  app.Name = "remember"
-  app.Usage = "utilize the awesome UI that is the command line to create Google Calendar events. Maybe if you use it you'll put your life in order :)"
-  app.Action = func(c *cli.Context) {
-    println(c.Args().First())
-  }
+
+    app := cli.NewApp()
+    app.Name = "remember"
+    app.Usage = "utilize the awesome UI that is the command line to create Google Calendar events. Maybe if you use it you'll put your life in order :)"
+    app.Action = func(c *cli.Context) {
+    
+        println(c.Args().First())
+
+    }
 
   app.Commands = []cli.Command{
   {
-      Name: "add",
-      Usage: "add an event to the calendar",
-      Action: func(c *cli.Context) {
+    
+    Name: "add",
+    Usage: "add an event to the calendar",
+    Action: func(c *cli.Context) {
   	
 
-	calendarService, err := calendar.New("https://www.googleapis.com/auth/calendar")
+	     calendarService, err := calendar.New("https://www.googleapis.com/auth/calendar")
 
-        event := &calendar.Event{
+       event := &calendar.Event{
       	
-        Summary: c.Args().First(),
-      	Location: "",
-      	Description: c.Args().First(),
-      	Start: &calendar.EventDateTime{
+       Summary: c.Args().First(),
+       Location: "",
+       Description: c.Args().First(),
+       Start: &calendar.EventDateTime {
+
             DateTime: "2016-05-28T09:00:00-07:00",
         	  TimeZone: "America/Detroit",
-      	},
 
-      	End: &calendar.EventDateTime{
+       },
+
+       End: &calendar.EventDateTime{
+
             DateTime: "2016-05-28T17:00:00-07:00",
         		TimeZone: "America/Detroit",
+
       		},
       		
-    	  }
+    	 }
 
-    	calendarId := "primary"
+    	 calendarId := "primary"
     	
 	event, err = srv.events.insert(calendarId, event).Do()
 
